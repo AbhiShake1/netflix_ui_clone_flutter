93b30563cf91257b79373f463f87490d7b4da565
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-01-31 20:44:52 +0545

    fix data and model

--- lib/models/content_model.dart
+++ lib/models/content_model.dart
@@ -4,14 +4,14 @@ import 'package:meta/meta.dart';
 class Content {
   final String name;
   final String imageUrl;
-  final String titleImageUrl;
-  final String videoUrl;
-  final String description;
-  final Color color;
+  final String? titleImageUrl;
+  final String? videoUrl;
+  final String? description;
+  final Color? color;
 
   const Content({
-    @required this.name,
-    @required this.imageUrl,
+    required this.name,
+    required this.imageUrl,
     this.titleImageUrl,
     this.videoUrl,
     this.description,
